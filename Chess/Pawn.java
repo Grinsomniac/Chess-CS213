8bc7ecfd24c91f27a44297baeb545d4a5d5918f1
Grinsomniac <johnjason2094@gmail.com>
2023-10-16 11:59:39 -0400

    Added enpassant structure
    enPAssant boolean
    enpassant capture move logic in isMoveValid()
    needs new enPassantCapture(), separate from regular capture() method

--- Chess/Pawn.java
+++ Chess/Pawn.java
@@ -6,6 +6,9 @@ public class Pawn extends Piece {
     public Pawn(PieceFile pieceFile, int pieceRank, boolean isWhite) {
         super(pieceFile, pieceRank, isWhite);
     }
+
+    public static boolean enPassant = false;
+
     @Override
     public PieceType getWhitePieceType() { return PieceType.WP; }
     @Override
@@ -50,6 +53,7 @@ public class Pawn extends Piece {
             }
             // This is where we should check for promotion when reaching the last rank
             pawnPromo(newRank, newFile, piecesOnBoard, playerWhite, promotionPiece);
+            enPassant = false;///////////////////////////////////// enPassant Toggle - False (or False after 1 tile first move or subsequent moves)) ///////////////////////
             return true;
         } else if (rankDifference == 2 && fileDifference == 0 && ((isWhite && pieceRank == 2) || (!isWhite && pieceRank == 7)) && fileDirection == 0) {
             // Initial move: two squares forward from the second rank (based on whether it's a white or black pawn)
@@ -60,6 +64,7 @@ public class Pawn extends Piece {
                     return false;
                 }
             } 
+            enPassant = true;/////////////////////////////////////////////////////////// enPassant Toggle ///////////////////////////////////////////////////////
             return true;
         } else if (rankDifference == 1 && fileDifference == 1 && pieceFile.ordinal() + fileDirection == newFile.ordinal()) {
             // Capturing a piece diagonally
@@ -81,11 +86,36 @@ public class Pawn extends Piece {
                     }
                 }
             }
+            //////////////////////////  New Code - EnPassant Capture  ////////////////////////////////////
+            // Check for en passant capture
+            if (enPassant && (playerWhite && pieceRank == 5) || (!playerWhite && pieceRank == 4)) {
+                // Determine the direction of movement for en passant capture
+                int enPassantRank = (playerWhite) ? (pieceRank + 1) : (pieceRank - 1);
+                int enPassantFile = newFile.ordinal();
+
+                // Check if there's an opponent's pawn at the en passant square
+                for (ReturnPiece piece : piecesOnBoard) {
+                    if (piece.pieceRank == enPassantRank && piece.pieceFile == PieceFile.values()[enPassantFile]) {
+                        // Capture the opponent's pawn en passant
+                        capture(pieceFile, pieceRank, newFile, newRank, piecesOnBoard);//////// - >>>>> NEEDS A NEW CAPTURE METHOD IN THIS CLASS FOR SPECICIFALLY ENPASSANT
+                        // Remove the captured piece from the board
+                        piecesOnBoard.remove(piece);
+                        pawnPromo(newRank, newFile, piecesOnBoard, playerWhite, promotionPiece);
+                        return true;
+                    }
+                }
+            }
+            //////////////////////////  New Code - EnPassant Capture  ////////////////////////////////////  
         }
         // The move is not valid for a pawn
         return false;
     }
 
+    public void enPassantCapture(){
+
+    }
+
+    
     public void pawnPromo(int newRank, PieceFile newFile, ArrayList<ReturnPiece> piecesOnBoard, boolean playerWhite, char promotionPiece ){
         // This is where we should check for promotion when reaching the last rank
         if ((newRank == 1 && !playerWhite) || (newRank == 8 && playerWhite)) {
